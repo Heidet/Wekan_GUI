68385ad5d3473103bfca74cccd3806377fd75984
unknown <a.heidet90400@gmail.com>
2021-07-16 11:42:35 +0200

    Modification des frame tkinter avec les buttons et intégration des futur add par panel

--- Wekan_gui/home.py
+++ Wekan_gui/home.py
@@ -24,169 +24,227 @@ boards = api.get_user_boards()
 
 
 root = tk.Tk()
-tree = ttk.Treeview(root)
-frame = tk.Frame(root)
+# tree = ttk.Treeview(root)
+# frame = tk.Frame(root)
+
+class Scrollable(tk.Frame):
+    """
+       Make a frame scrollable with scrollbar on the right.
+       After adding or removing widgets to the scrollable frame,
+       call the update() method to refresh the scrollable area.
+    """
+
+    def __init__(self, frame, width=16):
+
+        scrollbar = tk.Scrollbar(frame, width=width)
+        scrollbar.pack(side=tk.RIGHT, fill=tk.Y, expand=False)
+
+        self.canvas = tk.Canvas(frame, yscrollcommand=scrollbar.set)
+        self.canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+        scrollbar.config(command=self.canvas.yview)
+
+        self.canvas.bind('<Configure>', self.__fill_canvas)
+
+        # base class initialization
+        tk.Frame.__init__(self, frame)
+
+        # assign this obj (the inner frame) to the windows item of the canvas
+        self.windows_item = self.canvas.create_window(0,0, window=self, anchor=tk.NW)
+
+
+    def __fill_canvas(self, event):
+        "Enlarge the windows item to the canvas width"
+
+        canvas_width = event.width
+        self.canvas.itemconfig(self.windows_item, width = canvas_width)
+
+    def update(self):
+        "Update the canvas and the scrollregion"
+
+        self.update_idletasks()
+
+
+class ScrollbarFrame(tk.Frame):
+    """
+    Extends class tk.Frame to support a scrollable Frame 
+    This class is independent from the widgets to be scrolled and 
+    can be used to replace a standard tk.Frame
+    """
+    def __init__(self, parent, **kwargs):
+        tk.Frame.__init__(self, parent, **kwargs)
+
+        # The Scrollbar, layout to the right
+        vsb = tk.Scrollbar(self, orient="vertical")
+        vsb.pack(side="right", fill="y")
+
+        # The Canvas which supports the Scrollbar Interface, layout to the left
+        self.canvas = tk.Canvas(self, borderwidth=0, background="#ffffff")
+        self.canvas.pack(side="left", fill="both", expand=True)
+
+        # Bind the Scrollbar to the self.canvas Scrollbar Interface
+        self.canvas.configure(yscrollcommand=vsb.set)
+        vsb.configure(command=self.canvas.yview)
+
+        # The Frame to be scrolled, layout into the canvas
+        # All widgets to be scrolled have to use this Frame as parent
+        self.scrolled_frame = tk.Frame(self.canvas, background=self.canvas.cget('bg'), width=350, height=240)
+        self.canvas.create_window((4, 4), window=self.scrolled_frame, anchor="nw")
+
+        # Configures the scrollregion of the Canvas dynamically
+        self.scrolled_frame.bind("<Configure>", self.on_configure)
+
+    def on_configure(self, event):
+        """Set the scroll region to encompass the scrolled frame"""
+        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
 
 
 class GetAllBoards():
 
-	def board_selected(self, event):
-		for selected_item in tree.selection():
-			item = tree.item(selected_item)
-			record = item['values']
-			# showinfo(title='Information',
-			# message=','.join(record))
-			
+	def treeview_boards(self):   
+
+		root.geometry('400x350')
+		root['bg']='#2980b9'
+
+		label = Label(root, text='Liste Dashboards', font=('Helvetica bold',20), fg="white", bg="#2980b9")
+		label.pack(pady=20)
+
+		sbf = ScrollbarFrame(root, width=350, height=240,bg="#2980b9")
+		sbf.pack()
+		sbf.pack_propagate(0)
+		sbf.columnconfigure(index=0, weight=1)
+		sbf.columnconfigure(index=2, weight=1)
+		sbf.rowconfigure(index=0, weight=1)
+		sbf.rowconfigure(index=2, weight=1)
+		frame = sbf.scrolled_frame
+
+
+		def func(name):
+			print('item',name[0][0])
+			print('item',name[0][1])
 
 			board_data={
-				"title": record[0],
-				"_id": record[1],
+				"title": name[0][0],
+				"_id": name[0][1],
 				}
-			
+			sbf.destroy() 
+			label.destroy()
 			listcards = models.Board(api, board_data)
 			listcards.get_cardslists()
 			viewlist = GetBoardListCard()
 			viewlist.view_lists(board_data)
 
+		def funcadd(name):
+			print(name)
 
-	def treeview_boards(self):   
-		root.title('Liste Dashboards')
-		root.geometry('300x250')
-		root['bg']='#2980b9'
+		TkinterCustomButton(master=frame, fg_color="#999", hover_color="#999", width=250, height=80, text='(+) Ajouter un Dashboard ',command=functools.partial(funcadd)).pack(padx=30, pady=20)
 
-		treeScroll = ttk.Scrollbar(tree)
-		treeScroll.configure(command=tree)
-		tree.configure(yscrollcommand=treeScroll.set)
+		for item in boards:
+			data = []
+			title = item.idandtitle()[0]
+			idboard = item.idandtitle()[1]
+			data.append([f' {title}', f'{idboard}'])
+			button = TkinterCustomButton(master=frame, width=250, height=80, text=title, command=functools.partial(func,data))
+			button.pack(pady=10)
+			# data.append((f' {title}', f'{idboard}'))
 
-		tree['columns']=('Name')
-
-		tree.column('#0', width=0, stretch=NO) 
-		tree.column('Name', anchor=CENTER, width=240)
-		tree.heading('#0', text='', anchor=CENTER)
-		tree.heading('Name', text='Nom Dashboard', anchor=CENTER)
-		tree.bind('<<TreeviewSelect>>', self.board_selected)
-		tree.grid(row=0, column=0, sticky='nsew')
-		data = []
-
-
-		for board in boards:
-			name = board.idandtitle()[0]
-			idboard = board.idandtitle()[1]
-			data.append((f' {name}', f'{idboard}'))
-		for i in data:
-			tree.insert('', "end", values=i)
-		
-		tree.pack()
 		root.mainloop()
 
 
 class GetBoardListCard(GetAllBoards): 
 
-	def lists_selected(self, event):
-		for lists_selected in tree.selection():
-			item = tree.item(lists_selected)
-			record = item['values']
-			# showinfo(title='Information',
-			# message=','.join(record))
-			
-			# print('title =>',record[0])
-			# print('_id =>',record[1])
-			# print('boardid =>',record[2])
+	def view_lists(self, board_data): 
+		root.geometry('400x350')
+		root['bg']='#2980b9'
 
+		label = Label(root, text='liste des vues', font=('Helvetica bold',20), fg= "white",bg="#2980b9")
+		label.pack(pady=20)
+
+		sbf = ScrollbarFrame(root, width=350, height=240)
+		sbf.pack()
+		sbf.pack_propagate(0)
+		sbf.columnconfigure(index=0, weight=1)
+		sbf.columnconfigure(index=2, weight=1)
+		sbf.rowconfigure(index=0, weight=1)
+		sbf.rowconfigure(index=2, weight=1)
+		frame = sbf.scrolled_frame
+
+		def func(name):
+			print(name)
+			print('item',name[0][0])
+			print('item',name[0][1])
+			print('item',name[0][2])
 			cardslist_data={
-				"title": record[0],
-				"_id": record[1],
-				"boardid": record[2]
+				"title": name[0][0],
+				"_id": name[0][1],
+				"boardid": name[0][2]
 				}
-
-			board = record[2]
+			sbf.destroy() 
+			label.destroy()
+			board = name[0][2]
 			cardslist = models.Cardslist(api, board, cardslist_data)
 			cardsdata = cardslist.get_cards()
-			# print('cardsdata=>',cardsdata)
 			listcards = GetCardsOfList()
 			listcards.view_cards(cardslist_data,cardsdata)
 
-	def view_lists(self, board_data):  
-
-		for i in tree.get_children():
-			tree.delete(i)
-
-		root.title('Liste')
-		root.geometry('300x250')
-		root['bg']='#2980b9'
-
-		treeScroll = ttk.Scrollbar(tree)		
-		treeScroll.configure(command=tree)
-		tree.configure(yscrollcommand=treeScroll.set)
-
-		tree['columns']=('lists')
-
-		tree.column('#0', width=0, stretch=NO) 
-		tree.column('lists', anchor=CENTER, width=240)
-		tree.heading('#0', text='', anchor=CENTER)
-		tree.heading('lists', text='Listes', anchor=CENTER)
-		tree.bind('<<TreeviewSelect>>', self.lists_selected)
-		tree.grid(row=0, column=0, sticky='nsew')
-
-		listcards = models.Board(api, board_data)
-		cardslists_data = listcards.get_cardslists()
 		data = []
 		boardid = board_data["_id"]
+		listcards = models.Board(api, board_data)
+		cardslists_data = listcards.get_cardslists()
 
-		# print('boardid =>',boardid)
+		def funcadd(name):
+			print(name)
 
-		for i in cardslists_data: 
-			# print('listcards_id =>', i["_id"])
-			# print('listcards_title =>', i["title"])
-			id = i["_id"]
-			title = i["title"]
+		TkinterCustomButton(master=frame, fg_color="#999", hover_color="#999", width=250, height=80, text='(+) Ajouter une liste ',command=functools.partial(funcadd)).pack(padx=30, pady=20)
+
+		for item in cardslists_data:
+			data = []
+			id = item["_id"]
+			title = item["title"]
 			data.append((f' {title}', f'{id}', f'{boardid}'))
+			button = TkinterCustomButton(master=frame, width=250, height=80, text=title, command=functools.partial(func,data))
+			button.pack(pady=10)
 
-		for i in data:
-			tree.insert('', "end", values=i)
 
-		tree.pack()
-		root.mainloop()
 	
-class GetCardsOfList(): 
+class GetCardsOfList(GetAllBoards): 
 
 	def cards_selected(self, event):
 		pass
 
 	def view_cards(self, cardslist_data, cardsdata):  
-		tree.delete(*tree.get_children())
-		tree.destroy()
+
+
 		root.geometry('400x350')
 		titlelistcard = cardslist_data['title']
-		Label(root, text=titlelistcard, font=('Helvetica bold',20), fg= "white",bg="#2980b9").pack(pady=20)
-		# frame = Frame(root,  width=100, height=100)
-		# frame.pack(fill=None, expand=False)
+		Label(root, text=titlelistcard, font=('Helvetica bold',20), fg= "white",bg="#2980b9").pack(padx=30, pady=20)
 
-
-		frame = tk.Frame(root, bg='#ffffff', width=350, height=240)
-		frame.pack()
-		frame.pack_propagate(0)
-		frame.columnconfigure(index=0, weight=1)
-		frame.columnconfigure(index=2, weight=1)
-		frame.rowconfigure(index=0, weight=1)
-		frame.rowconfigure(index=2, weight=1)
+		sbf = ScrollbarFrame(root, width=350, height=240)
+		sbf.pack()
+		sbf.pack_propagate(0)
+		sbf.columnconfigure(index=0, weight=1)
+		sbf.columnconfigure(index=2, weight=1)
+		sbf.rowconfigure(index=0, weight=1)
+		sbf.rowconfigure(index=2, weight=1)
+		frame = sbf.scrolled_frame
 						
-
 		def func(name):
-			print (name)
+			print(name)
 
+		def funcadd(name):
+			print(name)
+
+		TkinterCustomButton(master=frame, fg_color="#999", hover_color="#999", width=250, height=80, text='(+) Ajouter une Card ',command=functools.partial(funcadd)).pack(padx=30, pady=20)
 
 		for item in cardsdata:
 			title = item['title']
 			button = TkinterCustomButton(master=frame, width=250, height=80, text=title,command=functools.partial(func,item))
-			#  width=30, height=2,  bg="#2980b9", 
 			button.pack(pady=10)
 
 
-		root.mainloop()
-
-	
 
 
-tkhome = GetAllBoards()
-tkhome.treeview_boards()
+
+if __name__ == "__main__":
+	tkhome = GetAllBoards()
+	tkhome.treeview_boards()
